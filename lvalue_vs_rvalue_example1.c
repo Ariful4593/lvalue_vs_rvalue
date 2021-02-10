0f0baa2d92e5e488311c9b6bd8cef3fc46485478
Ariful4593 <arifkhagoria2015@gmail.com>
2021-02-10 09:15:05 +0600

    Rvalue and Lvalue example

---
+++ lvalue_vs_rvalue_example1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+enum COLOR {RED, GREEN, BLUE};
+int main()
+{
+    int a,b;
+    enum COLOR c;
+    a = 4;
+    b = 2;
+    4 = a;
+    (a + 2) = 3;
+    b = (a + 2);
+    a + b = a - b;
+    c = RED;
+    GREEN = c;
+}
